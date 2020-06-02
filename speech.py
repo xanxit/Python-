8011e4e3d5925b2c3d6e85eff8bf45a8ec27893a
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-02 17:27:16 +0530

    Convert Text to speech

---
+++ speech.py
@@ -0,0 +1,7 @@
+from gtts import gTTS 
+import os 
+mytext = 'My Name is Sanchit'
+language = 'en'
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+myobj.save("welcome.mp3") 
+os.system("mpg321 welcome.mp3")
