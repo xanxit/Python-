b2f1b95b6cfb28d199b28d6692f884ffe7ff217e
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-02 19:20:59 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS #import the gTTS package
 import os 
-mytext = 'My Name is Sanchit'
+mytext = 'My Name is Sanchit'#write the text you need to convert to speech
 language = 'en'
 myobj = gTTS(text=mytext, lang=language, slow=False) 
 myobj.save("welcome.mp3") 
