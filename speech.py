7721199dfcc962f9cf8fde243b1cb867cedb4fef
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-06-02 19:15:49 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -4,4 +4,4 @@ mytext = 'My Name is Sanchit'
 language = 'en'
 myobj = gTTS(text=mytext, lang=language, slow=False) 
 myobj.save("welcome.mp3") 
-os.system("mpg321 welcome.mp3")
+os.system("welcome.mp3")
